8b3cd9c486ba39ec4cf16c36f73f4f391e6a84db
TimofeevAndrei <i.pika4u@gmail.com>
2023-03-03 16:04:00 +0700

    add search

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import vk_api
+from vk_access_token import access_token
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+vk = vk_api.VkApi(token=access_token)
+session_api = vk.get_api()
+
+SEX = 1
+AGE_FROM = 18
+AGE_TO = 40
+CITY = 1
+
+params = {'count': 1000,
+          'city': CITY,
+          'sex': SEX,
+          'age_from': AGE_FROM,
+          'age_to': AGE_TO,
+          'fields': 'can_write_private_message',
+          'has_photo': 1}
+
+users = session_api.users.search(**params)
+# print(users)
+avatar_links = {}
+for i in users['items']:
+    # print(i['is_closed'])
+    if i['is_closed'] == False and i['can_write_private_message'] == 1:
+        print(i['id'], i['last_name'], i['first_name'])
+        photo = session_api.photos.get(owner_id=i['id'], album_id='profile', count=3, photo_sizes='x', extended=1)
+        for y in photo['items']:
+            if y['likes']['count'] in avatar_links.keys():
+                avatar_links.update({y['likes']['count'] + y['date']: y['sizes'][-1]})
+            else:
+                avatar_links.update({y['likes']['count']: y['sizes'][-1]})
+    else:
+        pass
+
+print(avatar_links)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
