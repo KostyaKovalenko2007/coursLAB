19dee277d9ecca3964ebdcbff636d7eb328d0680
srtkkv <kovalenkokv@gmail.com>
2023-03-09 19:13:45 +0300

    добавил запись в базу поиска и не доделал пошаговый проход результатов

--- db.py
+++ db.py
@@ -1,35 +1,32 @@
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey,Boolean,JSON
-from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, Boolean, JSON
+from sqlalchemy.orm import sessionmaker, declarative_base, Query
 # from os import getenv
 import json
+
 Base = declarative_base()
 
-
-DIALECT = 'postgresql'
-USERNAME = 'postgres'
-PASSWORD = 'dimkanet'
-HOST = 'localhost'
-PORT = 5432
-DATABASE = 'VKBOT'
-
-# dialect+driver://username:password@host:port/database
-DSN = f"{DIALECT}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 class Client(Base):
     __tablename__ = 'clients'
     id = Column(Integer, primary_key=True)
-    vkID = Column(String,unique=True)
-    creteria = Column(JSON) #{"city:"","age":"","gender":"", "other":""}
-    currentSearchID  = Column(Integer, ForeignKey("SearchResults.id"))
-    def __init__(self, client_id:str,creteria:json):
+    vkID = Column(String, unique=True)
+    creteria = Column(JSON)  # {"city:"","age":"","gender":"", "other":""}
+    currentSearchID = Column(Integer, ForeignKey("SearchResults.id"), nullable=True)
+
+    def __init__(self, client_id: str, creteria: json):
         self.vkID = client_id
-        self.creteria=creteria
+        self.creteria = creteria
+        self.currentSearchID
 
     def __repr__(self):
         return f'<{self.__class__.__name__} #{self.id}>'
+    def get_current(self):
+        return self.currentSearchID
 
-
-
+    def set_serchID(self,id):
+        self.currentSearchID=id
 
 
 class SearchResult(Base):
@@ -47,30 +44,35 @@ class SearchResult(Base):
     def view(self):
         # формирует  cловарик с ответа ботом
         pass
+    def __init__(self, ClientID, vkID, fio, imgs=[]):
+        self.vkID = vkID
+        self.clientID = ClientID
+        self.fio = fio
+        self.img1 = imgs[0] if len(imgs) > 0 else ''
+        self.img2 = imgs[1] if len(imgs) > 1 else ''
+        self.img3 = imgs[2] if len(imgs) > 2 else ''
 
-    def __init__(self, name):
-        self.name = name
-    def refresh_search(self,client:Client, data:list ):
-        #Метод удаляет старые результаты поиска (кроме тех которые отмечены как Like/Dislike
+    def refresh_search(self, client: Client, data: list):
+        # Метод удаляет старые результаты поиска (кроме тех которые отмечены как Like/Dislike
         # Наполняет новой выборкой передаваемой в виде списка из словарей
         # устанавливает в профиле клиент значение первого из поисковой выборки
         # не добавляет которые уже помечены как Like/Dislike (если такие попадутся)
         pass
 
     def get_next_searchID(self):
-        #метод возвращает следующий идентификатор профиля из поискового запроса двигаясь последовательно перебирая профили по поисковому запросу
-        #возвращает следующий профиль и меняет сслыку currentSearchID
+        # метод возвращает следующий идентификатор профиля из поискового запроса двигаясь последовательно перебирая профили по поисковому запросу
+        # возвращает следующий профиль и меняет сслыку currentSearchID
         pass
+
 
 class Favorite(Base):
     __tablename__ = 'Favorites'
     id = Column(Integer, primary_key=True)
     ClientID = Column(Integer, ForeignKey("clients.id"))
     SearchID = Column(Integer, ForeignKey("SearchResults.id"))
-    like = Column(Boolean,default=True)
+    like = Column(Boolean, default=True)
 
-
-    def __init__(self, serch_item:SearchResult,like=True):
+    def __init__(self, serch_item: SearchResult, like=True):
         self.ClientID = serch_item.id
         self.SearchID = serch_item.vkID
         self.like = like
@@ -78,16 +80,19 @@ class Favorite(Base):
     def __repr__(self):
         return f'<Favorite#{self.SearchID} tag#{self.like}>'
 
-    def like_correction(self,like=True):
-        #Корректирует мнение если оно изменилось
+    def like_correction(self, like=True):
+        # Корректирует мнение если оно изменилось
         pass
 
+
 class BotDB():
+    session = None
+
     def __init__(self):
-        #engine = create_engine("postgresql://vk_bot_lab:Qw123456@192.168.88.4/vkbot")
-        #Session = sessionmaker(bind=engine)
+        # engine = create_engine("postgresql://vk_bot_lab:Qw123456@192.168.88.4/vkbot")
+        # Session = sessionmaker(bind=engine)
         # DSN = getenv('dsn')
-        self.engine = create_engine(DSN)
+        self.engine = create_engine(os.getenv('dsn'))
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
@@ -97,11 +102,42 @@ class BotDB():
         Base.metadata.create_all(self.engine)
         print('Tables created')
         pass
-    def create_client(self,vkID,creteria):
-        return Client(client_id=vkID,creteria=creteria)
-    def get_client_by_vkID(self,vkID:str):
-        return self.session.query(Client(vkID=vkID))
-    def put_search(self,ClientID:Client,SearchResults:list):
-        #Удаляет предыдущий поиск из таблицы SearchResults для клиента
-        #обнуляет
+
+    def create_client(self, vkID, creteria: json):
+        client = Client(client_id=vkID, creteria=creteria)
+        try:
+            self.session.add(client)
+            self.session.commit()
+            return 1, "Client registered"
+        except:
+            return 0, f"Can't create record for\n VKID={vkID}\n Criteria={creteria} "
+
+    def get_client_criterias(self, vkID):
+        res: Query = self.session.query(Client).filter(Client.vkID == str(vkID))
+        if res.count() == 1:
+            return json.loads(res.first().creteria)
+        else:
+            return None
+
+    def update_client_creteria(self, vkID: str, creteria: json):
+        self.session.query(Client).filter(Client.vkID == vkID).update({Client.creteria: creteria})
+        self.session.commit()
+
+    def get_client_by_vkID(self, vkID: str):
+        return self.session.query(Client(vkID=str(vkID))).first()
+
+    def put_search(self, ClientID: Client, SearchResults: list):
+        # Удаляет предыдущий поиск из таблицы SearchResults для клиента
+        # обнуляет
+        client = self.session.query(Client).filter(Client.vkID == str(ClientID)).first()
+        self.session.query(SearchResult).filter(
+            SearchResult.clientID==client.id).delete()  # TODO нужно исключить имеющиеся завписи в Favorites
+        for profile in SearchResults:
+            item = SearchResult(ClientID=client.id,
+                                    vkID=str(profile['id']),
+                                    fio=f'{profile["first_name"]} {profile["last_name"]}',
+                                    imgs=list(profile['photos'])
+                                )
+            self.session.add(item)
+        self.session.commit()
         pass
--- vk_bot.py
+++ vk_bot.py
@@ -12,11 +12,15 @@ class vkBOT():
     session = None
     api = None
     db: BotDB = None
-    def __init__(self, db:BotDB):
+    sex = {1:'Женский', 2:'Мужской'}
+    priv_api = None
+    def __init__(self, db: BotDB):
         self.session = vk_api.VkApi(token=getenv('token'))
         self.longpoll = VkLongPoll(self.session)
-        self.db = db
+        self.db: BotDB = db
+        self.vk = self.session.get_api()
         self.api = self.session.get_api()
+        self.priv_api = vk_api.VkApi(token=getenv('priv_token')).get_api()
 
     def write_msg(self, user_id, message, keyboard=None):
         post = {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)}
@@ -25,60 +29,103 @@ class vkBOT():
         self.session.method('messages.send', post)
 
     def register_client_profile(self, user_id):
-        vkapi = self.session.get_api()
-        info = vkapi.users.get(user_ids=user_id, fields='city, sex, bdate')
-        criteria = json.dumps(info[0])
-        print(criteria)
+        info = self.api.users.get(user_ids=user_id, fields='city, sex, bdate')[0]
+        if info.get('sex') == 1:
+            info['sex'] = 2
+        elif info.get('sex') == 2:
+            info['sex'] = 1
+
+        result, msg = self.db.create_client(vkID=user_id, creteria=json.dumps(info))
+        print(msg)
+        return result
+
+    def search_by_client_criteria(self, Client_id):
+        creteria = self.db.get_client_criterias(vkID=Client_id)
+
+        params = {'count': 20,
+                  'city': creteria['city'].get('id'),
+                  'sex': creteria['sex'],
+                  # 'age_from': AGE_FROM, #TODO надо подять как обрабатывать возраст
+                  # 'age_to': AGE_TO,
+                   'fields': 'can_write_private_message',
+                  'has_photo': 1}
+        users = self.priv_api.users.search(**params)
+        search_list = []
+        for user in users['items']:
+            photos = self.get_user_photos(vkID=user['id'])
+            if photos != []:
+                search_list.append({'id':user['id'],
+                                    'first_name':user['first_name'],
+                                    'last_name':user['last_name'],
+                                    'photos': photos
+                                    })
+        self.db.put_search(ClientID=Client_id, SearchResults=search_list)
+
         pass
 
-    def search_by_client_criteria(self,Client_id):
+    def get_next_in_searchResults(self, ClientID):
+        client = self.db.get_client_by_vkID(vkID=ClientID)
+        self.db.session.query(SearchResult).filter(SearchResult.clientID==client.id and SearchResult.id > client.currentSearchID)
+
         pass
 
-    def get_next_in_searchResults(self,ClientID):
+    def set_like_dislike(self, clientID, like=False):
 
-        pass
-
-    def set_like_dislike(self,clientID,like=False):
         pass
 
     def run(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
-               if event.to_me:
-                    self.register_client_profile(event.user_id)
+                if event.to_me:
                     request = event.text
                     print(event.user_id, event.text)
                     if request == "start":
-                        keyboard =VkKeyboard()
+                        keyboard = VkKeyboard()
                         keyboard.add_button("Регистрация", VkKeyboardColor.PRIMARY, payload='{\"button\":\"1\"}')
                         keyboard.add_button("Поиск", VkKeyboardColor.POSITIVE)
                         keyboard.add_button("Выход", VkKeyboardColor.NEGATIVE)
                         self.write_msg(event.user_id, "button", keyboard)
                     elif request == "Выход":
-                        keyboard =VkKeyboard(one_time=True)
+                        keyboard = VkKeyboard(one_time=True)
                         keyboard.add_button("Пока!", VkKeyboardColor.POSITIVE)
                         keyboard.get_empty_keyboard()
                         self.write_msg(event.user_id, "button", keyboard)
                     elif request == "Поиск":
-                        keyboard =VkKeyboard(inline=True)
+                        self.search_by_client_criteria(Client_id=event.user_id)
+                        keyboard = VkKeyboard(inline=True)
                         keyboard.add_button("Нравится", VkKeyboardColor.POSITIVE)
                         keyboard.add_button("Не нравится", VkKeyboardColor.NEGATIVE)
                         keyboard.add_button("Следующее", VkKeyboardColor.PRIMARY)
                         self.write_msg(event.user_id, "button", keyboard)
                     elif request == "Регистрация":
-                        keyboard =VkKeyboard()
+                        self.register_client_profile(user_id=event.user_id)
+                        info = self.db.get_client_criterias(vkID=str(event.user_id))
+                        self.write_msg(event.user_id, "Вас зарегистрировали", None)
+                        self.write_msg(event.user_id,
+                                       "Ваши поисковые критерии:\n"\
+                                       f"Город: {info['city'].get('title')}\n"\
+                                       f"Пол: {self.sex[info.get('sex')]}\n"\
+                                       f"Возраст: {info.get('bdate')}", None)
+                        keyboard = VkKeyboard()
                         keyboard.add_button("Введите город", VkKeyboardColor.POSITIVE)
                         keyboard.add_button("Введите возраст", VkKeyboardColor.POSITIVE)
-                        self.write_msg(event.user_id, "button", keyboard)
-                    elif request == "привет":
-                        self.write_msg(event.user_id, f"Хай, {event.user_id}")
-                    elif request == "пока":
-                        self.write_msg(event.user_id, "Пока((")
+                        # self.write_msg(event.user_id, "button", keyboard)
                     else:
-                        self.write_msg(event.user_id, "Не поняла вашего ответа...")
+                        self.write_msg(event.user_id, "Команда не распознана начните с команды 'start'")
+    def get_user_photos(self,vkID):
+        photo_dict = {}
+        try:
+            for photo in self.priv_api.photos.get(owner_id=vkID,album_id='profile',extended=1)['items']:
+                photo_dict[photo['sizes'][-1]['url']] = photo['likes']['count']
+            return  sorted(photo_dict,key=lambda x:x[1], reverse=True)[:3] # sorted list of photo by Likes, limited by to 3
+            #print(self.priv_api.users.get(user_ids=vkID,)['first_name'])
+        except:
+            return []
 
 
 if __name__ == '__main__':
     bd = BotDB()
+    # bd.create_tables()
     vkbot = vkBOT(bd)
     vkbot.run()
+
